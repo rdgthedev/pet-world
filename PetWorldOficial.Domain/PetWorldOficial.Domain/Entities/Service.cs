22e4b8e9e158954c2dc31e9be1c1f4b9fe4ce4f5
Rodrigo <rodrigo.ferreiradaslv@gmail.com>
2024-05-14 15:28:52 -0300

    feat: Criada e implementada a entidade de serviços

---
+++ PetWorldOficial.Domain/PetWorldOficial.Domain/Entities/Service.cs
@@ -0,0 +1,34 @@
+﻿using PetWorldOficial.Domain.Common;
+
+namespace PetWorldOficial.Domain.Entities;
+
+public class Service : Entity
+{
+    public string Name { get; private set; }
+    public double Price { get; private set; }
+    public string ImageUrl { get; private set; }
+    
+    public Service(
+        string name, 
+        double price, 
+        string imageUrl)
+    {
+        Name = name;
+        Price = price;
+        ImageUrl = imageUrl;
+    }
+
+    public bool IsValid()
+    {
+        if (string.IsNullOrEmpty(Name) || Name.Length < 3)
+            return false;
+        
+        if (Price <= 0)
+            return false;
+
+        if (string.IsNullOrEmpty(ImageUrl))
+            return false;
+
+        return true;
+    }
+}
